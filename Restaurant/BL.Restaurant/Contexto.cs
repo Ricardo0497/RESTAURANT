bc976a6f578bfda02abab333d0e4fe9d0f11ae45
Laptop <Laptop@RAAZ>
2020-03-01 12:28:52 -0600

    Creando La Clase Contexto

---
+++ Restaurant/BL.Restaurant/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Restaurant
+{
+   public class Contexto: DbContext //heredar clases enC#
+    {
+        public Contexto() : base()
+        {
+
+        }
+
+        public DbSet<Producto> Productos { get; set; } //Crea la Lista  Jalando los Datos de Producto BL
+    }
+
+}
