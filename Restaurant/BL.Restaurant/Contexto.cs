708a605c8292e1ab6748476944dfeb4aea3ffd38
Laptop <Laptop@RAAZ>
2020-03-03 09:56:08 -0600

    Asignando nombre de Base de Datos

--- Restaurant/BL.Restaurant/Contexto.cs
+++ Restaurant/BL.Restaurant/Contexto.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BL.Restaurant
 {
-   public class Contexto: DbContext //heredar clases enC#
+    public class Contexto : DbContext //heredar clases enC#
     {
-        public Contexto() : base()
+        public Contexto() : base("Menu")
         {
 
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)   //Crear Modelo y base de dato
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();// Remueva las tablas pluralizadas
         }
 
         public DbSet<Producto> Productos { get; set; } //Crea la Lista  Jalando los Datos de Producto BL
